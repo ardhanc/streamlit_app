ef25287145de39c13888cd68ce6f16a60e03a0aa
Anand Janarthanan <105019501+ardhanc@users.noreply.github.com>
2022-05-05 17:55:04 -0400

    Update streamlit_app.py
    Added multi select list and added index on fruits column in the csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 myfruitlist = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+myfruitlist = myfruitlist.set_index('Fruit')
+
+streamlit.multiselect('Pick some fruits:',list(myfruitlist.index)
 streamlit.dataframe(myfruitlist)
