693bfd95751e013b3d6a791d236f688363fea9a5
Anand Janarthanan <105019501+ardhanc@users.noreply.github.com>
2022-05-05 18:03:50 -0400

    Update streamlit_app.py
    added code to filter based on selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 myfruitlist = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 myfruitlist = myfruitlist.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:",list(myfruitlist.index),['Avocado','Strawberries'])
-streamlit.dataframe(myfruitlist)
+fruitselected = streamlit.multiselect("Pick some fruits:",list(myfruitlist.index),['Avocado','Strawberries'])
+fruittoshow = myfruitlist.loc(fruitselected)
+streamlit.dataframe(fruittoshow)
